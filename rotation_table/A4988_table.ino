5e79b4a309e108aa70c41ad03ca1d921b4438511
Pranith-Dot <prbh1609@colorado.edu>
2025-03-07 13:27:15 -0700

    Update A4988_table.ino

--- rotation_table/A4988_table.ino
+++ rotation_table/A4988_table.ino
@@ -10,45 +10,43 @@
 unsigned int initial_time;
 unsigned int log_time;
 unsigned int counter_time;
-
-A4988 stepper(200, DIRECTION_PIN, MOTOR_PIN, MS1, MS2, MS3);
+A4988 stepper(400, DIRECTION_PIN, MOTOR_PIN, MS1, MS2, MS3);
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN,INPUT);
-  stepper.begin(1/6,1);
+  stepper.begin(20,1); //set RPM 
   
 
 }
 void RecordData(int b){
-  if(millis()- log_time >= 50){ //log every 50 ms
-          Serial.print(millis()-initial_time);       
-          Serial.print(",\t");
-          Serial.println(b*5);
-          log_time=millis();
+ Serial.print(millis()-initial_time);       
+ Serial.print(",\t");
+ Serial.println(b*5);
        
-        }
-  }
+ }
   
 
 
 
 void loop() {
-  if(digitalRead(BUTTON_PIN)==HIGH){ //if button is pressed, move 5 degrees every 5 seconds 
+//  if(digitalRead(BUTTON_PIN)==HIGH){ 
     initial_time = millis();
     log_time = millis();
-    
+    static int count = 1;
+    Serial.print("Entering revolution number:");
+    Serial.println(count);
     for(int b = 0; b<72; b++){
-    Serial.print("Entering new Iteration:");
+    Serial.print("Entering iteration:");
     Serial.println(b);
+    
     stepper.rotate(5);
+    RecordData(b);
+    delay(2000);
    
-      counter_time = millis(); 
-      while(millis()-counter_time<5000){ //wait for 5 seconds after 5 degrees
-       RecordData(b+1);
-     }
    }
+      count++;
   
- }
+ //}
 
 }
