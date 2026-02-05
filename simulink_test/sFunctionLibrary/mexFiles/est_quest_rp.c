874abb7cd9c3623c67cfe018ee97cc974e80ed6f
Adam Yarrow <adam.yarrow@colorado.edu>
2026-02-05 13:43:58 -0700

    Testing ABS tol rather than raw value for DCOMM singularity issue.

--- simulink_test/sFunctionLibrary/mexFiles/est_quest_rp.c
+++ simulink_test/sFunctionLibrary/mexFiles/est_quest_rp.c
@@ -26,7 +26,7 @@
  * | See matlabroot/simulink/src/sfuntmpl_doc.c for a more detailed template |
  *  -------------------------------------------------------------------------
  *
- * Created: Thu Feb 05 13:24:22 2026
+ * Created: Thu Feb 05 13:38:32 2026
  */
 
 #define S_FUNCTION_LEVEL               2
